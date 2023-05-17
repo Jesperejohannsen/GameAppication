2c90683eee796529ca98ff2ba7bc989e187047a9
Jesperejohannsen <you@example.com>
2023-05-17 17:37:50 +0200

    Adding Dark Mode Switch

--- GameApp/src/App.tsx
+++ GameApp/src/App.tsx
@@ -9,15 +9,15 @@ function App() {
       lg: '"nav nav" "main aside"', // Wider than 1024px
 
      }}>
-      <GridItem area="nav" bg="coral">
+      <GridItem area="nav">
         Nav
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>
