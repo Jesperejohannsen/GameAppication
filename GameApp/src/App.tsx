1a2d559683997aeae379b84ff34b7e02fa913125
Jesperejohannsen <you@example.com>
2023-05-18 21:12:09 +0200

    Building game cards.

--- GameApp/src/App.tsx
+++ GameApp/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside">
-          Aside
+          
         </GridItem>
       </Show>
       <GridItem area="main">
