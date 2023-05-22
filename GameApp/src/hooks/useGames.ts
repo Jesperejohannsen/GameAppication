4f315c2b0c0e754871f38ed383af7491c75b3684
Jesperejohannsen <you@example.com>
2023-05-22 21:33:06 +0200

    Refactor. Making a placeholder for the cards.

--- GameApp/src/hooks/useGames.ts
+++ GameApp/src/hooks/useGames.ts
@@ -26,20 +26,23 @@ interface FetchGameResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
 
     useEffect(() => {
 
         const controller = new AbortController();
-
+        setLoading(true);
         apiClient
             .get<FetchGameResponse>("/games", {signal: controller.signal})
-            .then((response) => setGames(response.data.results));
+            .then((response) => {
+                setGames(response.data.results);
+                setLoading(false);});
 
         return () => controller.abort();
     }, []);
 
-    return { games, error };
+    return { games, error, loading };
 }
 
 export default useGames;
